dd744960b50b22a0c4b70ffd0a97052f13c04c78
Peter Szujewski <pszujewski@gmail.com>
2017-06-09 17:27:01 -0400

    updates setup for mongo and user model

---
+++ server/models/user.js
@@ -0,0 +1,9 @@
+// Steps
+// 1 Review how other team did oauth 
+// 2 Try to implement yourself without looking at past code
+// 3 Start here with user model and setting up db
+
+// Signing in should route to profile from where they can route to list of 
+// availble chat messages, which then routes to indidual chat rooms
+// so implement react router to handle all this and facilitate chat 
+// functionality development
