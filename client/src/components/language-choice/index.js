1623320c3e2793558f5044a238c411e5a29a2ec6
Peter Szujewski <pszujewski@gmail.com>
2017-07-01 16:38:58 -0400

    Improves language selection ux

--- client/src/components/language-choice/index.js
+++ client/src/components/language-choice/index.js
@@ -15,40 +15,7 @@ export class LanguageChoice extends Component{
   constructor(props) {
     super(props);
 
-    this.state = {
-      options: []
-    };
-
     this.accessToken = Cookies.get('accessToken');
-  }
-  
-  componentDidMount() {
-    const optionsJsx = this.mapOverOptions(options, this.props);
-    this.updateOptionsJsx(optionsJsx);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.user && nextProps.user) {
-      const optionsJsx = this.mapOverOptions(options, nextProps);
-      this.updateOptionsJsx(optionsJsx);
-    }
-  }
-
-  updateOptionsJsx(optionsJsx) {
-    this.setState({
-      options: optionsJsx
-    })
-  }
-
-  handleChange(event){
-    event.preventDefault();
-    if (event.target.value === 'default-option') {
-      return;
-    }
-    if (this.props.forDictaphone) {
-      return this.updateDictaphoneLanguage(event.target.value);
-    }
-    return this.updateDefaultLanguage(event.target.value);
   }
 
   updateDictaphoneLanguage(language) {
@@ -61,47 +28,52 @@ export class LanguageChoice extends Component{
     }
   }
 
+  handleChange(event) {
+    // event.preventDefault();
+    if (event.target.value === 'default-option') {
+      return;
+    }
+    if (this.props.forDictaphone) {
+      return this.updateDictaphoneLanguage(event.target.value);
+    }
+    return this.updateDefaultLanguage(event.target.value);
+  }
+
   mapOverOptions(options, props) {
     return options.map((o, index) => {
-      if (props.user) {
-        if (!props.forDictaphone && props.user.defaultLanguage === o.value) {
-          return <option key={index} name={o.name} value={o.value} defaultValue>{o.name}</option>
-        }
-      }
-      return <option key={index} name={o.name} value={o.value} defaultValue>{o.name}</option>
+      return <option key={index} name={o.name} value={o.value}>{o.name}</option>
     });
   }
 
-evalWindowSize() {
-  if (this.props.windowWidth < 900) {
+  evalWindowSize() {
+    if (this.props.windowWidth < 900) {
+      return (
+        <select onChange={(e) =>{this.handleChange(e)}}>
+          <option value='default-option'>Choose a Language</option>
+          { this.mapOverOptions(options, this.props) }
+        </select>
+      );
+    }
     return (
-      <select onChange={(e) =>{this.handleChange(e)}}>
+      <Input s={12} 
+        type='select' 
+        label="Language Choice"  
+        onChange={(e) => {this.handleChange(e)}}
+      >
         <option value='default-option'>Choose a Language</option>
-        { this.state.options }
-      </select>
+        { this.mapOverOptions(options, this.props) }
+      </Input>
     );
   }
-  return (
-    <Input s={12} type='select' label="Language Choice" defaultValue='Choose a Language' 
-      onChange={(e) =>
-      {this.handleChange(e)}}
-    >
-      <option value='default-option'>Choose a Language</option>
-      { this.state.options }
-    </Input>
-  );
-}
 
-render(){ 
-    return(
+  render() { 
+    return (
         <Row className='language-choice-row'>
           { this.evalWindowSize() }
         </Row>
     );
   }
+
 }
-const mapStateToProps = state => ({
-  userLanguage: state.userLanguage,
-  user: state.userData.user
-});
-export default connect(mapStateToProps)(windowSize(LanguageChoice));
+
+export default connect()(windowSize(LanguageChoice));
